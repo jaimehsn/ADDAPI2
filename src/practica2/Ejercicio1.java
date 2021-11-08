c4cc0ba1ced430a372d9e95cd2937f4a6899a956
jaimehsn <jaimehsn@gmail.com>
2021-11-08 22:57:08 +0100

    Ejercicio1 Iterativo y Test1

--- src/practica2/Ejercicio1.java
+++ src/practica2/Ejercicio1.java
@@ -23,14 +23,26 @@ public class Ejercicio1 {
 	}
 
 	public static String ejercicio1Iterativo(Integer a, Integer b, Integer c) {
-		String salida;
-		while (a > 3 && b > 3 && c > 3) {
-
+		String salida = "";
+		while (a >= 3 && b >= 3 && c >= 3) {
+			if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+//				salida = Integer.toString(a * b * c) + ejercicio1NoFinal(a / 2, b - 2, c / 2);
+				salida += Integer.toString(a * b * c);
+				a = a / 2;
+				b = b - 2;
+				c = c / 2;
+			} else {
+//				salida = Integer.toString(a + b + c) + ejercicio1NoFinal(a / 3, b - 3, c / 3);
+				salida += Integer.toString(a + b + c);
+				a = a / 3;
+				b = b - 3;
+				c = c / 3;
+			}
 		}
 		if (a < 3 || b < 3 || c < 3) {
-			salida = String.format("(%s)", (a + b + c));
+			salida += String.format("(%s)", (a + b + c));
 		} else {
-			salida = String.format("(%s)", (a * b * c));
+			salida += String.format("(%s)", (a * b * c));
 		}
 		return salida;
 	}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,13 +1,32 @@
 package test;
 
+import java.util.List;
+
 import practica2.Ejercicio1;
+import us.lsi.common.Files2;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Ejercicio1.ejercicio1NoFinal(20, 40, 80));
-		System.out.println(Ejercicio1.ejercicio1Final(20, 40, 80));
+		Test1();
+	}
+
+	public static void Test1() {
+		List<String> lineas = Files2.linesFromFile("ficheros/PI2Ej1DatosEntrada.txt");
+		String[] dato;
+		for (String linea : lineas) {
+			dato = linea.split(",");
+			System.out.println(String.format("(a, b, c) = (%s, %s, %s)", dato[0], dato[1], dato[2]));
+			System.out.println(String.format("Sol. Rec. No final: \t%s", Ejercicio1.ejercicio1NoFinal(
+					Integer.parseInt(dato[0]), Integer.parseInt(dato[1]), Integer.parseInt(dato[2]))));
+			System.out.println(String.format("Sol. Iterativo: \t%s", Ejercicio1.ejercicio1Iterativo(
+					Integer.parseInt(dato[0]), Integer.parseInt(dato[1]), Integer.parseInt(dato[2]))));
+			System.out.println(String.format("Sol. Rec. Final: \t%s", Ejercicio1
+					.ejercicio1Final(Integer.parseInt(dato[0]), Integer.parseInt(dato[1]), Integer.parseInt(dato[2]))));
+			System.out.println("_____________________________________________________________________________________");
+
+		}
 	}
 
 }
