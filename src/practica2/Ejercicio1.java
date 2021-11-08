784a57c9a96a63f6b0400909a390e4746e5762cd
jaimehsn <jaimehsn@gmail.com>
2021-11-08 17:41:04 +0100

    Ejercicio 1 Recursivo RecursivoFinal

---
+++ src/practica2/Ejercicio1.java
@@ -0,0 +1,59 @@
+package practica2;
+
+public class Ejercicio1 {
+
+	public static String ejercicio1NoFinal(Integer a, Integer b, Integer c) {
+		String salida;
+
+		if (a < 3 && b < 3 && c < 3) {
+			salida = String.format("(%s)", (a * b * c));
+		} else {
+			if (a < 3 || b < 3 || c < 3) {
+				salida = String.format("(%s)", (a + b + c));
+			} else {
+				if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+					salida = Integer.toString(a * b * c) + ejercicio1NoFinal(a / 2, b - 2, c / 2);
+				} else {
+					salida = Integer.toString(a + b + c) + ejercicio1NoFinal(a / 3, b - 3, c / 3);
+				}
+			}
+		}
+
+		return salida;
+	}
+
+	public static String ejercicio1Iterativo(Integer a, Integer b, Integer c) {
+		String salida;
+		while (a > 3 && b > 3 && c > 3) {
+
+		}
+		if (a < 3 || b < 3 || c < 3) {
+			salida = String.format("(%s)", (a + b + c));
+		} else {
+			salida = String.format("(%s)", (a * b * c));
+		}
+		return salida;
+	}
+
+	public static String ejercicio1Final(Integer a, Integer b, Integer c) {
+		String salida = "";
+
+		salida = ejercicio1Final(a, b, c, salida);
+
+		return salida;
+	}
+
+	private static String ejercicio1Final(Integer a, Integer b, Integer c, String salida) {
+		if (a < 3 && b < 3 && c < 3) {
+			salida += String.format("(%s)", (a * b * c));
+		} else if (a < 3 || b < 3 || c < 3) {
+			salida += String.format("(%s)", (a + b + c));
+		} else if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+			salida = ejercicio1Final(a / 2, b - 2, c / 2, salida.concat(Integer.toString(a * b * c)));
+		} else {
+			salida = ejercicio1Final(a / 3, b - 3, c / 3, salida.concat(Integer.toString(a + b + c)));
+		}
+		return salida;
+	}
+
+}
---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+import practica2.Ejercicio1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Ejercicio1.ejercicio1NoFinal(20, 40, 80));
+		System.out.println(Ejercicio1.ejercicio1Final(20, 40, 80));
+	}
+
+}
